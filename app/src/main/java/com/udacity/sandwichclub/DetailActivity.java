9499efda1c5089635924093f0d808aa056a40399
Gilad Nadav <gilad.nadav@kaltura.com>
2019-04-19 17:29:53 +0300

    add image place holder

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -65,6 +65,7 @@ public class DetailActivity extends AppCompatActivity {
         populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .placeholder(R.drawable.placeholder)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getName().getMainName());
